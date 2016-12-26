ca5a2cb10120e960c6cc409692790ab78a4105a0
Ruslan <matveev.ruslan@gmail.com>
2016-12-26 20:01:30 +0300

    cleanup

--- index.js
+++ index.js
@@ -41,64 +41,41 @@ function requestPassword(prompt, ret) {
 	authorize(prompt, ret);
 }
 
-function info(imagePath, ret) {
-	if (typeof imagePath !== 'string') imagePath = String(imagePath);
+function info(path, ret) {
+	if (typeof path !== 'string') path = String(path);
 	if (typeof ret !== 'function') ret = function(){};
-	execFile('/usr/bin/hdiutil', [
-		'info',
-		'-plist'
-	], function(error, result) {
-		if (error) return ret(error);
-		result = plist.parse(result).images.filter(image => image['image-path'] === imagePath)[0];
-		if (!result) return ret(null);
-		result = result['system-entities'].filter(entity => entity.hasOwnProperty('mount-point'))[0];
-		if (!result) return ret(null);
-		ret(null, result['mount-point'], result['dev-entry']);
+	execFile('/usr/bin/hdiutil', ['info', '-plist'], function(error, result) {
+		if (!error && (result = plist.parse(result).images.filter(image => image['image-path'] === path)[0]) &&
+			(result = result['system-entities'].filter(entity => !!entity['mount-point'])[0])) {
+			ret(null, result['mount-point'], result['dev-entry']);
+		} else ret(error);
 	});
 }
 
-function isEncrypted(imagePath, ret) {
-	execFile('/usr/bin/hdiutil', [
-		'isencrypted',
-		imagePath,
-		'-plist'
-	], function(error, result) {
-		if (!error) try {
-			result = plist.parse(result).encrypted;
-		} catch (exception) {
-			error = exception;
-			result = false;
-		}
-		ret(error, result);
+function isEncrypted(path, ret) {
+	if (typeof path !== 'string') path = String(path);
+	if (typeof ret !== 'function') ret = function(){};
+	execFile('/usr/bin/hdiutil', ['isencrypted', path, '-plist'], function(error, result) {
+		ret(error, !error && plist.parse(result).encrypted);
 	});
 }
 
-function attach(imagePath, ret, options) {
-	if (typeof imagePath !== 'string') imagePath = String(imagePath);
+function attach(path, ret, options) {
+	if (typeof path !== 'string') path = String(path);
 	if (typeof ret !== 'function') ret = function(){};
-	info(imagePath, function(error, mountPath, devicePath) {
-		if (error) return ret(error);
-		if (mountPath) return ret(null);
-		isEncrypted(imagePath, function(error, encrypted) {
+	if (typeof options !== 'object') options = {};
+	info(path, function(error, mountPath, devicePath) {
+		if (error || mountPath) return ret(error);
+		isEncrypted(path, function(error, encrypted) {
 			if (error) return ret(error);
-
-			if (typeof options !== 'object') options = {};
 
 			var prompt = options.prompt,
 				password = options.password,
-				args = ['attach', imagePath, '-plist', '-stdinpass'],
+				args = ['attach', path, '-plist', '-stdinpass'],
 				repeatTimes = (encrypted && typeof password !== 'string' && options.repeat);
 
-			if (typeof repeatTimes !== 'number' ||
-				isNaN(repeatTimes) ||
-				repeatTimes < 0 ||
-				repeatTimes % 1) {
-				repeatTimes = 0;
-			}
-
-			if (typeof prompt !== 'string') {
-				prompt = 'Enter password to access ' + path.basename(imagePath);
-			}
+			if (typeof prompt !== 'string') prompt = 'Enter password to access ' + path.basename(path);
+			if (typeof repeatTimes !== 'number' || isNaN(repeatTimes) || repeatTimes < 0 || repeatTimes % 1) repeatTimes = 0;
 
 			if (options.readonly) args.push('-readonly');
 			if (options.nobrowse) args.push('-nobrowse');
@@ -107,52 +84,30 @@ function attach(imagePath, ret, options) {
 			forAsync(function(repeat) {
 
 				var proc = execFile('/usr/bin/hdiutil', args, function(error, result, errorMsg) {
-
-					if (repeatTimes > repeat.iteration && errorMsg.indexOf('Authentication error') !== -1)
-						return repeat();
-
-					if (error) ret(error); else ret(null);
-
-				});
+					if (repeatTimes > repeat.iteration && errorMsg.indexOf('Authentication error') !== -1) repeat();
+					else if (error) ret(error);
+					else ret(null);
+				}), stdin = proc.stdin;
 
 				if (encrypted) {
-
-					proc.stdin.setEncoding('UTF-8');
-
-					if (typeof password === 'string') {
-						proc.stdin.write(password);
-						proc.stdin.end();
-					}
-
+					stdin.setEncoding('UTF-8');
+					if (typeof password === 'string') stdin.write(password), stdin.end();
 					else requestPassword(prompt, function(password, cancelled) {
-
-						if (!cancelled) {
-							proc.stdin.write(password);
-							proc.stdin.end();
-						}
-
-						else proc.kill('SIGINT');
-
-
+						if (cancelled) proc.kill('SIGINT');
+						else stdin.write(password), stdin.end();
 					});
-
-
-
 				}
 
 			}, 0, Infinity);
-
-
 		});
 	});
 }
 
-function detach(imagePath, ret, force) {
-	if (typeof imagePath !== 'string') imagePath = String(imagePath);
+function detach(path, ret, force) {
+	if (typeof path !== 'string') path = String(path);
 	if (typeof ret !== 'function') ret = function(){};
-	info(imagePath, function(error, mountPath, devicePath) {
-		if (error) return ret(error);
-		if (!devicePath) return ret(null);
+	info(path, function(error, mountPath, devicePath) {
+		if (error || !devicePath) return ret(error);
 		var args = ['detach', devicePath];
 		if (force) args.push('-force');
 		execFile('/usr/bin/hdiutil', args, ret);
